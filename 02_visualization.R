b7527fd1de3116b02b4617a129619cf8bdb16fcc
chiaraborelli <borellichiara9@gmail.com>
2025-03-04 17:51:02 +0100

    Update 02_visualization.R

--- 02_visualization.R
+++ 02_visualization.R
@@ -12,3 +12,4 @@ im.list()
 b2 = im.import("sentinel.dolomites.b2.tif") #associo ad im.import la banda 2, l'immagine che ottengo mostra che tutto ciò che è giallo riflette tantissimo il blu
 cl=colorRampPalette(c("black", "dark grey","light grey"))(100) #questi colori sono tre elementi di un'array (vettore), per metterli insieme devo usare la c di concatenait.100 sta per tutti i colori che ci sono
 plot(b2, col=cl)
+# r colors su internet, it give you all the colors you are interest in
