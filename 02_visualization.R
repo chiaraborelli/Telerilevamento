dddf4a9a076c086b1011ab6b4c8313502958b493
chiaraborelli <borellichiara9@gmail.com>
2025-03-04 17:45:17 +0100

    Create 02_visualization.R

---
+++ 02_visualization.R
@@ -0,0 +1,14 @@
+#R code for visualizing satellite data
+
+#install.packages("devtools")
+library(devtools)
+install_github("ducciorocchini/imageRy")
+
+library(terra)
+library(imageRy)
+
+im.list()
+# For the whole course we are going to make use of = onstead of <-
+b2 = im.import("sentinel.dolomites.b2.tif") #associo ad im.import la banda 2, l'immagine che ottengo mostra che tutto ciò che è giallo riflette tantissimo il blu
+cl=colorRampPalette(c("black", "dark grey","light grey"))(100) #questi colori sono tre elementi di un'array (vettore), per metterli insieme devo usare la c di concatenait.100 sta per tutti i colori che ci sono
+plot(b2, col=cl)
