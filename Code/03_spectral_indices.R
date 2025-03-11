bac946cf3d529c35745d2b2e92addc3e6300a91c
chiaraborelli <borellichiara9@gmail.com>
2025-03-11 17:42:51 +0100

    Update 03_spectral_indices.R

--- Code/03_spectral_indices.R
+++ Code/03_spectral_indices.R
@@ -23,3 +23,25 @@ im.plotRGB(mato1992, r=2, g=1, b=3)
 im.plotRGB(mato1992, r=2, g=3, b=1)
 
 #import the 2006 image of the Mato Grosso area
+
+#ora facciamo con il satellite ast
+mato06=im.import("matogrosso_ast_2006209_lrg.jpg")
+mato06=flip(mato06)
+plot(mato06)
+
+im.multiframe(1,2)
+im.plotRGB(mato1992, r=2, g=3, b=1)
+im.plotRGB(mato06, r=2, g=3, b=1)
+
+#possiamo anche fare il plot di una singola banda per vedere il range di riflettanza
+#radiometric resolution
+plot(mato1992[[1]], col=inferno(100))
+plot(mato06[[1]], col=inferno(100))
+
+
+#DVI (difference vegetation index)
+# Tree: NIR=255, red=0, DVI=NIR-red=255
+#Stressed tree: NIR=100, red=20, DVI=NIR-red=80
+#la morale è che questo indice, una volta fatta la differenza ci dà una diretta informazione dello stato di saalute della vegetazione
+
+
