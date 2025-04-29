7ebc7d6ffedd7508fc0542d9697d60d7c36a5677
chiaraborelli <borellichiara9@gmail.com>
2025-04-29 17:29:55 +0200

    Create 13_colorblindness.R

---
+++ Code/13_colorblindness.R
@@ -0,0 +1,31 @@
+# Code to solve colorblindness problems
+
+#Packages
+library(terra)
+
+
+#Installing cblindplot
+library(devtools)
+install_github("ducciorocchini/cblindplot")
+library(cblindplot)
+
+#Importing data
+setwd("C:/Users/chiar/Desktop/UNIBO/Magistrale/Telerilevamento")
+vinicunca=rast("vinicunca.jpg")
+
+plot(vinicunca)
+vinicunca = flip(vinicunca)
+plot(vinicunca)
+
+#simulating color blindness
+im.multiframe(1,2)
+im.plotRGB(vinicunca, r=1, g=2, b=3, title="Standard Vision")
+im.plotRGB(vinicunca, r=1, g=2, b=3, title="Protanopia")
+
+rainbow = rast("rainbow.jpg")
+plot(rainbow)
+rainbow = flip(rainbow)
+plot(rainbow)
+cblind.plot(rainbow, cvd="deuteranopia")
+cblind.plot(rainbow, cvd="protanopia")
+cblind.plot(rainbow, cvd="")
